a3d26fd73f67964c54ee7cda0362527d35a8992b
Cha14ka <cha14ka@yandex.ru>
2024-04-01 11:23:55 +0300

    фикс видеопамяти

--- plugins/status.py
+++ plugins/status.py
@@ -24,15 +24,15 @@ class Status:
 		mem = psutil.virtual_memory()
 		MB = 1024 * 1024
 
-		vram = subprocess.run('nvidia-smi', shell=True, capture_output=True).stdout.decode()
-		vram = re.findall('W\s\/\s.+?W.+?(\d+).+?(\d+)', vram)[0]
-
 		text += '\n&#8195;ОЗУ:\n&#8195;&#8195;Всего: '+str(int(mem.total / MB))+'MB\n'
 		text += '&#8195;&#8195;Использовано: '+str(int((mem.total - mem.available) / MB))+'MB\n'
 		text += '&#8195;&#8195;Свободно: '+str(int(mem.available / MB))+'MB\n'
 		text += '&#8195;&#8195;Использовано ботом: '+str(int(psutil.Process().memory_info().rss / MB))+'MB\n\n'
 
 		if os.path.exists('/usr/bin/nvidia-smi'):
+			vram = subprocess.run('nvidia-smi', shell=True, capture_output=True).stdout.decode()
+			vram = re.findall('W\s\/\s.+?W.+?(\d+).+?(\d+)', vram)[0]
+
 			text += f'&#8195;Видеопамять:\n&#8195;&#8195;Всего: {vram[1]} MB\n'
 			text += f'&#8195;&#8195;Использовано: {vram[0]} MB\n'
 			text += f'&#8195;&#8195;Свободно: {int(vram[1])-int(vram[0])} MB\n'
